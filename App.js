3886c9f00900974aa29372c91e76d12ea7c6312b
SergioMartinAlvaro <smaral.sergio@gmail.com>
2020-08-18 16:37:55 +0200

    First Commit

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Header from './components/Header';
+import { nanoid } from 'nanoid/async/index.native'
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    {id:  nanoid(), text: 'Milk'},
+    {id:  nanoid(), text: 'Eggs'},
+    {id:  nanoid(), text: 'Bread'},
+    {id:  nanoid(), text: 'Juice'}
+  ]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View style={styles.container}
     >
-      <Text>Universal React with Expo</Text>
+      <Header title='Shopping List'/>
     </View>
   );
 }
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
